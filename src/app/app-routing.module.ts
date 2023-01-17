24a0eb232a703742352836b3bb6a07f490f02047
Hawa656 <hawacoulibaly656@gmail.com>
2023-01-17 08:13:21 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,36 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
+    //loadChildren: () => import('./tab-bar/tab-bar.module').then( m => m.TabBarPageModule)
+
+     //loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'page1',
+    loadChildren: () => import('./page1/page1.module').then( m => m.Page1PageModule)
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'tab-bar',
+    loadChildren: () => import('./tab-bar/tab-bar.module').then( m => m.TabBarPageModule)
+  },
+  {
+    path: 'fruits',
+    loadChildren: () => import('./fruits/fruits.module').then( m => m.FruitsPageModule)
+  },
+  {
+    path: 'accueil',
+    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   },
 ];
 
